ce33b100a3476f4346fdea5d82d2fa5b86670405
Collect_diamond <3066703409@qq.com>
2022-11-30 08:33:36 +0800

    modified:   README.md 	modified:   public/config.js

--- public/config.js
+++ public/config.js
@@ -1,33 +1,30 @@
 window.Config = {
+	// 显示标题
+	SiteName: "Collect_diamond 站点监测界面",
 
-  // 显示标题
-  SiteName: 'IMSYY 站点监测',
+	// UptimeRobot Api Keys
+	// 支持 Monitor-Specific 和 Read-Only
+	ApiKeys: ["u1926791-11757ce4be3c589d752bb7f0"],
 
-  // UptimeRobot Api Keys
-  // 支持 Monitor-Specific 和 Read-Only
-  ApiKeys: [
-    'u1022423-6ffcda436b183008644c691c',
-  ],
+	// 日志天数
+	CountDays: 60,
 
-  // 日志天数
-  CountDays: 60,
+	// 是否显示检测站点的链接
+	ShowLink: true,
 
-  // 是否显示检测站点的链接
-  ShowLink: true,
-
-  // 导航栏菜单
-  Navi: [
-    {
-      text: '主页',
-      url: 'https://www.imsyy.top/'
-    },
-    {
-      text: '博客',
-      url: 'https://blog.imsyy.top/'
-    },
-    {
-      text: 'GitHub',
-      url: 'https://github.com/imsyy/'
-    },
-  ],
+	// 导航栏菜单
+	Navi: [
+		{
+			text: "个人主页",
+			url: "https://collect-diamond.top/",
+		},
+		{
+			text: "备用镜像",
+			url: "https://collect-diamond.tk/",
+		},
+		{
+			text: "GitHub",
+			url: "https://github.com/Collect-diamond/",
+		},
+	],
 };
